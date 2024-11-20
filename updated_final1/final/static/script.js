d373448726c876b841cda853d93fd7460dfb9031
SagarGopinath <125530220+SagarGopinath@users.noreply.github.com>
2024-11-20 20:42:31 +0530

    Add files via upload
    final commit after adding maps and search bar

---
+++ updated_final1/final/static/script.js
@@ -0,0 +1,60 @@
+let slideIndex = 0;
+showSlides();
+
+// Automatic Slideshow
+function showSlides() {
+    let slides = document.getElementsByClassName("slide");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex - 1].style.display = "block";  
+    setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
+
+// Manual Slide Control
+function changeSlide(n) {
+    slideIndex += n;
+    let slides = document.getElementsByClassName("slide");
+    if (slideIndex > slides.length) {slideIndex = 1}
+    if (slideIndex < 1) {slideIndex = slides.length}
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slides[slideIndex - 1].style.display = "block";  
+}
+let secondarySlideIndex = 0;
+showSecondarySlides(secondarySlideIndex);
+
+function changeSecondarySlide(n) {
+    showSecondarySlides(secondarySlideIndex += n);
+}
+
+function showSecondarySlides(n) {
+    const slides = document.getElementsByClassName("secondary-slide");
+    const maxVisible = 4;
+    if (n > slides.length - maxVisible) { secondarySlideIndex = 0; }
+    if (n < 0) { secondarySlideIndex = slides.length - maxVisible; }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (let i = secondarySlideIndex; i < secondarySlideIndex + maxVisible; i++) {
+        slides[i].style.display = "block";
+    }
+}
+
+function openModal() {
+  document.getElementById("termsModal").style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById("termsModal").style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == document.getElementById("termsModal")) {
+    closeModal();
+  }
+}
